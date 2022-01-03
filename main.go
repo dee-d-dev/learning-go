001089947565f26e90b58dd2ca83a163270b9391
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-01-03 18:54:31 +0100

    Learning Golang

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var eventName = "Go conference"
+	const eventTickets = 100
+
+	fmt.Println("hello people, Only", eventTickets, eventName, "is available")
+	fmt.Println("Get your",eventName, "tickets here")
+
+	
+}
